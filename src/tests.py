74005a2461c5d21616d4b27c755f1e29f8a019f7
Nathan <nzhang32@gmail.com>
2013-06-17 14:33:25 -0700

    Added random tests for Proxy<int>
    Signed-off-by: Nathan <nzhang32@gmail.com>

--- src/tests.py
+++ src/tests.py
@@ -1,7 +1,10 @@
 import unittest
+import random
+import operator
 from proxy import Proxy
+from objectproxy import ObjectProxy
 
-class TestMutable(unittest.TestCase):
+class TestProxy(unittest.TestCase):
     def setUp(self):
         self.testtypes = (
             int, str, tuple, list
@@ -10,6 +13,27 @@ class TestMutable(unittest.TestCase):
         for testtype in self.testtypes:
             self.assertIsInstance(Proxy(testtype)(),testtype)
         return True
+    def test_int(self):
+        IntProxy = Proxy(int)
+        tests = (
+            operator.iadd,
+            operator.iand,
+            operator.ifloordiv,
+            operator.ilshift,
+            operator.imod,
+            operator.imul,
+            operator.ior,
+            operator.ipow,
+            operator.irshift,
+            operator.itruediv,
+            operator.ixor
+            )
+        for test in tests:
+            for i in range(100):
+                a = b = IntProxy(random.randint(1,1000))
+                test(a,random.randint(1,1000))
+                self.assertEqual(a,b)
+
 
 if __name__ == '__main__':
     unittest.main()
